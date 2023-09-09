51673b2f78e98bd4a5ae102ebd628a2a0111f96c
José Santos <thecaasantos@gmail.com>
2023-09-09 01:33:46 -0300

    :wrench: chore: database connection config

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const app = express();
 require('dotenv').config();
 
 
+//Database Conection
+uri = process.env.DATABASE_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
 //Midlewares register
 app.use(express.json());
 
 
 //Server
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server ins running on port ${port}`);
