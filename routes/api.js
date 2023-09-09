20a32e211a2c8d85c9f6f38bdb5ea9389c1f2e5a
José Santos <thecaasantos@gmail.com>
2023-09-09 03:21:04 -0300

    :sparkles: feat: post route

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userControllers');
 
 router.get('/users', userController.getUsers);
 router.get('/users/:userId', userController.getOneUser);
+router.post('/user', userController.createUser);
 
 module.exports = router;
