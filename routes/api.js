8899f1cbf6f8450321c0b607b2be783e323758ed
José Santos <thecaasantos@gmail.com>
2023-09-12 13:11:49 -0300

    :sparkles: feat: create put and delete routes

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ const userController = require('../controllers/userControllers');
 
 router.get('/users', userController.getUsers);
 router.get('/users/:userId', userController.getOneUser);
-router.post('/user', userController.createUser);
+router.post('/users', userController.createUser);
+router.put('/users/:userId', userController.updateUser);
+router.delete('/users/:userId', userController.deleteUser);
 
 module.exports = router;
