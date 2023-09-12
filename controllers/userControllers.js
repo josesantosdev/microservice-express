91be118d1a0fc01a280920b9d1348501d3ebe8e5
José Santos <thecaasantos@gmail.com>
2023-09-12 13:12:15 -0300

    :sparkles: feat: create put and delete methods

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -49,3 +49,49 @@ module.exports.createUser = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+module.exports.updateUser = async (req, res) => {
+    const {name, email }  = req.body;
+    const userId = req.params.userId;
+
+    if (!name || !email) {
+        res.status(400).json({ error: 'Name and email are required' });
+    }
+
+    try {
+        const user = await User.findOne({ _id: userId}).exec();
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        await user.updateOne({name: name, email: email});
+        res.status(201).json({ updatedUser: user});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+module.exports.deleteUser = async (req, res) => {
+    userId = req.params.userId;
+
+    if (!ObjectId.isValid(userId)){
+        return res.status(400).json({ error: 'Invalid User' });
+    }
+
+    try {
+        const user = await User.findOne({_id: userId}).exec();
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.deleteOne({_id: userId});
+        res.status(201).json({ deletedUser: user});
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
